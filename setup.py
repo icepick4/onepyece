2c9667163a995cb5a03d28a8cacba23616a02cdd
Icepick <remi.jara@laposte.net>
2023-01-25 22:53:17 +0100

    init the project

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as file:
+    long_description = file.read()
+
+setuptools.setup(
+    name = "one-pyce",
+    version = "0.0.1",
+    author = "Rémi JARA",
+    author_email = "remi.jara4@gmail.com",
+    description = "A package to use the One-Piece API : https://api-onepiece.com/",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "package URL",
+    project_urls = {
+        "Bug Tracker": "package issues URL",
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.6"
+)
